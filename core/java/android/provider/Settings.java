2cb6adaa908d9021c2875e48540fdf5644250f7e
Laszlo David <laszlo.david@gmail.com>
2013-02-16 01:23:12 +0100

    Swap volume buttons when the screen is rotated (1/2)
    See: http://review.cyanogenmod.org/#/c/14530/
    
    Patch Set 8: Invert the orientation interpretations on tablets
    
    Change-Id: I201febc2827e6fbd347cb0e3f8d3347ad1bc487b
    
    Conflicts:
    
    	media/java/android/media/AudioManager.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	media/java/android/media/AudioManager.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4490,6 +4490,15 @@ public final class Settings {
          */
         public static final String WIFI_AUTO_CONNECT_TYPE = "wifi_auto_connect_type";
 
+         /**
+         * Swap volume buttons when the screen is rotated
+         * 0 - Disabled
+         * 1 - Enabled (screen is rotated by 90 or 180 degrees: phone, hybrid)
+         * 2 - Enabled (screen is rotated by 180 or 270 degrees: tablet)
+         * @hide
+         */
+        public static final String SWAP_VOLUME_KEYS_ON_ROTATION = "swap_volume_keys_on_rotation";
+
         /**
          * Whether wifi settings will connect to access point automatically when
          * network from mobile network transform to Wifi network
