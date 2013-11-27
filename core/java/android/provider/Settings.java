0e4c6e89408bb1276605f65a8070454ca958e834
sarbyn <sarbyn@gmail.com>
2013-11-27 13:05:20 +0100

    Development shortcut in StatusBar and Recent Panel
    It enables the wipe data and application force close option in the
    recent tasks list and in the notification area. These options will
    be available only if the Development shortcut option is enabled.
    
    Change-Id: Ia4e1d040e8eade5afe118dd62bc28c86381c03e6
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/menu/recent_popup_menu.xml
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/menu/notification_popup_menu.xml
    	packages/SystemUI/res/menu/recent_popup_menu_split.xml
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6450,6 +6450,7 @@ public final class Settings {
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
+<<<<<<< HEAD
          * Default theme to use.  If empty, use holo.
          * @hide
          */
@@ -6473,6 +6474,13 @@ public final class Settings {
          */
         public static final String DEFAULT_THEME_APPLIED_ON_FIRST_BOOT =
                 "default_theme_applied_on_first_boot";
+=======
+         * Whether to display the 'Wipe data' and 'Force close' options in the notification
+         * area and in the recent app list
+         * @hide
+         */
+        public static final String DEVELOPMENT_SHORTCUT = "development_shortcut";
+>>>>>>> 7598ec2... Development shortcut in StatusBar and Recent Panel
 
         /**
          * This are the settings to be backed up.
@@ -6527,7 +6535,8 @@ public final class Settings {
             UI_THEME_AUTO_MODE,
             PRIVACY_GUARD_DEFAULT,
             PRIVACY_GUARD_NOTIFICATION,
-            ADVANCED_REBOOT
+            ADVANCED_REBOOT,
+            DEVELOPMENT_SHORTCUT
         };
 
         /**
