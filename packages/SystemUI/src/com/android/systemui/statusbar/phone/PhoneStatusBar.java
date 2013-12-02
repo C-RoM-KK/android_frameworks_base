3050341bc8104acc72b295c0041c8e2ed82a5eef
xplodwild <xplod@ouverta.fr>
2013-12-02 19:26:10 +0100

    SystemUI: Fix notification header disappearing on tablets
    For some reason, the notification panel header has weird dimensions
    when it has anything else than its original ColorDrawable as
    background, and only on tablets.
    
    We workaround this by not changing the drawable until requested.
    
    Change-Id: Id76a5dcacf59c35b2e49681d6eeb4e828f7cbc58

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -58,6 +58,7 @@ import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.inputmethodservice.InputMethodService;
@@ -1616,6 +1617,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void setNotificationPanelHeaderBackground(final Drawable dw) {
         Drawable[] arrayDrawable = new Drawable[2];
+
+        if (dw instanceof BitmapDrawable) {
+            BitmapDrawable bdw = (BitmapDrawable) dw;
+            bdw.setGravity(Gravity.TOP);
+        }
+
+        if (!(dw instanceof BitmapDrawable) &&
+             !(mNotificationPanelHeader.getBackground() instanceof BitmapDrawable) &&
+             !(mNotificationPanelHeader.getBackground() instanceof TransitionDrawable)) {
+            return;
+        }
+
         arrayDrawable[0] = mNotificationPanelHeader.getBackground();
         arrayDrawable[1] = dw;
 
