d26b2d2ffd5d57975b3373e784cb21a3c3cff622
qqzhou <qqzhou@codeaurora.org>
2013-12-17 14:10:08 +0800

    FWBase - Add to support pause/resume download [1/2]
    We add pause and resume API here so that application can
    pause and resume downloads manually by DownloadManager.
    This contains two points:
    1. add pause and resume download APIs.
    2. add another paused reason for this download status.
    
    PS2/3:
    * Fix code.
    
    Change-Id: I8e7878312c1301aebe5dc4bc5e25ad2197e15741
    Signed-off-by: MURA Stefano [STELIX PippoX3] <ssspinni@gmail.com>

--- core/java/android/app/DownloadManager.java
+++ core/java/android/app/DownloadManager.java
@@ -266,6 +266,13 @@ public class DownloadManager {
      */
     public final static int PAUSED_UNKNOWN = 4;
 
+   /**
+    * Value of {@link #COLUMN_REASON} when the download is paused by manual.
+    *
+    * @hide
+    */
+    public final static int PAUSED_BY_MANUAL = 5;
+
     /**
      * Broadcast intent action sent by the download manager when a download completes.
      */
@@ -865,6 +872,7 @@ public class DownloadManager {
                     parts.add(statusClause("=", Downloads.Impl.STATUS_WAITING_TO_RETRY));
                     parts.add(statusClause("=", Downloads.Impl.STATUS_WAITING_FOR_NETWORK));
                     parts.add(statusClause("=", Downloads.Impl.STATUS_QUEUED_FOR_WIFI));
+                    parts.add(statusClause("=", Downloads.Impl.STATUS_PAUSED_BY_MANUAL));
                 }
                 if ((mStatusFlags & STATUS_SUCCESSFUL) != 0) {
                     parts.add(statusClause("=", Downloads.Impl.STATUS_SUCCESS));
@@ -1408,6 +1416,9 @@ public class DownloadManager {
                 case Downloads.Impl.STATUS_QUEUED_FOR_WIFI:
                     return PAUSED_QUEUED_FOR_WIFI;
 
+                case Downloads.Impl.STATUS_PAUSED_BY_MANUAL:
+                    return PAUSED_BY_MANUAL;
+
                 default:
                     return PAUSED_UNKNOWN;
             }
@@ -1466,6 +1477,7 @@ public class DownloadManager {
                 case Downloads.Impl.STATUS_WAITING_TO_RETRY:
                 case Downloads.Impl.STATUS_WAITING_FOR_NETWORK:
                 case Downloads.Impl.STATUS_QUEUED_FOR_WIFI:
+                case Downloads.Impl.STATUS_PAUSED_BY_MANUAL:
                     return STATUS_PAUSED;
 
                 case Downloads.Impl.STATUS_SUCCESS:
