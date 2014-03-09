b29c59c12e6668678bf8674f6d7cdaf54cc71330
Jmz <jmztaylor@gmail.com>
2014-03-09 08:28:00 +0100

    FWB: Allow disabling of FC dialogs (1/2)
    Change-Id: If87aa74ebb45849f6e5fe3a53ce9beeaac28f624
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8792,6 +8792,12 @@ public final class Settings {
         public static final String LOW_BATTERY_SOUND_TIMEOUT = "low_battery_sound_timeout";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
