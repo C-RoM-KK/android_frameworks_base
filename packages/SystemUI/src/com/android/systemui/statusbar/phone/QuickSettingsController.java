367b1ff8c0f6a03807a445da9298ab1a0643b9cc
Jacob Mueller <jacob.mueller.elz@gmail.com>
2014-05-14 22:09:34 +0200

    framework: Add compass tile (1/2)
    Add a compass tile to quick settings.
    Clicking on it enables the sensor and clicking again disables it.
    
    Screenshot: http://goo.gl/2frdpy
    
    Change-Id: Icd9d12929f0491f6958f30de139ef39bb4c2e037
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSUtils.java
    	packages/SystemUI/res/drawable/btn_ga_cancel_gesturing.xml
    	packages/SystemUI/res/values/crom_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -25,6 +25,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_BATTERY;
 import static com.android.internal.util.slim.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.slim.QSConstants.TILE_BRIGHTNESS;
 import static com.android.internal.util.slim.QSConstants.TILE_BUGREPORT;
+import static com.android.internal.util.mahdi.QSConstants.TILE_COMPASS;
 import static com.android.internal.util.slim.QSConstants.TILE_CONTACT;
 import static com.android.internal.util.slim.QSConstants.TILE_CUSTOM;
 import static com.android.internal.util.slim.QSConstants.TILE_CUSTOM_KEY;
@@ -81,6 +82,7 @@ import com.android.systemui.quicksettings.BatteryTile;
 import com.android.systemui.quicksettings.BluetoothTile;
 import com.android.systemui.quicksettings.BrightnessTile;
 import com.android.systemui.quicksettings.BugReportTile;
+import com.android.systemui.quicksettings.CompassTile;
 import com.android.systemui.quicksettings.ContactTile;
 import com.android.systemui.quicksettings.CustomTile;
 import com.android.systemui.quicksettings.ExpandedDesktopTile;
@@ -262,6 +264,8 @@ public class QuickSettingsController {
                 qs = new QuickRecordTile(mContext, this);
             } else if (tile.equals(TILE_SHAKE)) {
                 qs = new ShakeEventTile(mContext, this);
+            } else if (tile.equals(TILE_COMPASS)) {
+                qs = new CompassTile(mContext, this);
             } else if (tile.contains(TILE_CUSTOM)) {
                 qs = new CustomTile(mContext, this, findCustomKey(tile));
             } else if (tile.contains(TILE_CONTACT)) {
