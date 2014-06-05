50c403232770ef3af47f87f20e207baee4e14003
aways <aways@c-rom.org>
2014-06-05 02:00:46 +0200

    Revert "Bluetooth: Adds check to verify Bluetooth adapter state"
    This reverts commit fb9a50a2e1ea34a1e5ba123bec79e2fb7b1ea06e.

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -1752,7 +1752,6 @@ public final class BluetoothAdapter {
      */
     public void stopLeScan(LeScanCallback callback) {
         if (DBG) Log.d(TAG, "stopLeScan()");
-        if (getState() != STATE_ON) return;
         GattCallbackWrapper wrapper;
         synchronized(mLeScanClients) {
             wrapper = mLeScanClients.remove(callback);
