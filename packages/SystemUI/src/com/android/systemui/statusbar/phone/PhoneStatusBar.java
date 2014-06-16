c5369a002ad3ff53fa1b639854d6a081d2f5eb1d
Andy Mast <andy@cyngn.com>
2014-06-15 19:21:47 -0700

    Modify config to support app specific themes [1/4]
    Summary of Changes
    1. The configuration now supports each app to have their own theme.
    Each theme can include icons, styles (ie overlays) and font.
    
    2. The theme config is serialized into a json string and stored
    in secure settings.  This entry consolidates what was previously 3 separate
    keys (style, icon, font) into a single field.
    
    3. 'CustomTheme' was renamed to 'ThemeConfiguration'.
    
    Note: This commit is for the configuration. Additional work needs to be done to
    the framework and UI to support app specific themes.
    
    Change-Id: I6f72f542829578201c57c54786456486f6a6e2e3
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	services/java/com/android/server/am/ActivityManagerService.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -48,7 +48,7 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.res.Configuration;
-import android.content.res.CustomTheme;
+import android.content.res.ThemeConfig;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Canvas;
@@ -375,6 +375,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     Runnable mPostCollapseCleanup = null;
 
     // status bar brightness control
+
     private boolean mBrightnessControl;
     private boolean mAnimatingFlip = false;
     private float mScreenWidth;
@@ -387,7 +388,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     // last theme that was applied in order to detect theme change (as opposed
     // to some other configuration change).
-    CustomTheme mCurrentTheme;
+    ThemeConfig mCurrentTheme;
     private boolean mRecreating = false;
 
     private StatusHeaderMachine mStatusHeaderMachine;
@@ -1067,9 +1068,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 .getDefaultDisplay();
         updateDisplaySize();
 
-        CustomTheme currentTheme = mContext.getResources().getConfiguration().customTheme;
+        ThemeConfig currentTheme = mContext.getResources().getConfiguration().themeConfig;
         if (currentTheme != null) {
-            mCurrentTheme = (CustomTheme)currentTheme.clone();
+            mCurrentTheme = (ThemeConfig)currentTheme.clone();
         }
 
         mCurrUiThemeMode = mContext.getResources().getConfiguration().uiThemeMode;
@@ -1848,7 +1849,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (DEBUG) Log.v(TAG, "addNavigationBar: about to add " + mNavigationBarView);
         if (mNavigationBarView == null) return;
 
-        CustomTheme newTheme = mContext.getResources().getConfiguration().customTheme;
+        ThemeConfig newTheme = mContext.getResources().getConfiguration().themeConfig;
         if (newTheme != null &&
                 (mCurrentTheme == null || !mCurrentTheme.equals(newTheme))) {
             // Nevermind, this will be re-created
@@ -4504,14 +4505,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         final Context context = mContext;
         final Resources res = context.getResources();
 
-        // detect theme change
-        CustomTheme newTheme = res.getConfiguration().customTheme;
+        // detect theme change.
+        ThemeConfig newTheme = res.getConfiguration().themeConfig;
         if (newTheme != null &&
                 (mCurrentTheme == null || !mCurrentTheme.equals(newTheme))) {
-            mCurrentTheme = (CustomTheme)newTheme.clone();
-            recreateStatusBar(false);
-            return;
-        }
+            mCurrentTheme = (ThemeConfig)newTheme.clone();
+            recreateStatusBar();
+        } else {
 
         // detect theme ui mode change
         int uiThemeMode = res.getConfiguration().uiThemeMode;
