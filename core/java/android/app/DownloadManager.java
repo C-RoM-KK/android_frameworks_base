5453b15c78888f2c5c036f9f759dd86c77ca609c
aways <aways@c-rom.org>
2014-06-20 01:49:17 +0200

    Revert "FWBase - Add to support pause/resume download [1/2]"
    This reverts commit 006cf2c4cb3096f08aa5222ae9d8d6b02391de35.

--- core/java/android/app/DownloadManager.java
+++ core/java/android/app/DownloadManager.java
@@ -266,13 +266,6 @@ public class DownloadManager {
      */
     public final static int PAUSED_UNKNOWN = 4;
 
-   /**
-    * Value of {@link #COLUMN_REASON} when the download is paused by manual.
-    *
-    * @hide
-    */
-    public final static int PAUSED_BY_MANUAL = 5;
-
     /**
      * Broadcast intent action sent by the download manager when a download completes.
      */
@@ -872,7 +865,6 @@ public class DownloadManager {
                     parts.add(statusClause("=", Downloads.Impl.STATUS_WAITING_TO_RETRY));
                     parts.add(statusClause("=", Downloads.Impl.STATUS_WAITING_FOR_NETWORK));
                     parts.add(statusClause("=", Downloads.Impl.STATUS_QUEUED_FOR_WIFI));
-                    parts.add(statusClause("=", Downloads.Impl.STATUS_PAUSED_BY_MANUAL));
                 }
                 if ((mStatusFlags & STATUS_SUCCESSFUL) != 0) {
                     parts.add(statusClause("=", Downloads.Impl.STATUS_SUCCESS));
@@ -1416,9 +1408,6 @@ public class DownloadManager {
                 case Downloads.Impl.STATUS_QUEUED_FOR_WIFI:
                     return PAUSED_QUEUED_FOR_WIFI;
 
-                case Downloads.Impl.STATUS_PAUSED_BY_MANUAL:
-                    return PAUSED_BY_MANUAL;
-
                 default:
                     return PAUSED_UNKNOWN;
             }
@@ -1477,7 +1466,6 @@ public class DownloadManager {
                 case Downloads.Impl.STATUS_WAITING_TO_RETRY:
                 case Downloads.Impl.STATUS_WAITING_FOR_NETWORK:
                 case Downloads.Impl.STATUS_QUEUED_FOR_WIFI:
-                case Downloads.Impl.STATUS_PAUSED_BY_MANUAL:
                     return STATUS_PAUSED;
 
                 case Downloads.Impl.STATUS_SUCCESS:
